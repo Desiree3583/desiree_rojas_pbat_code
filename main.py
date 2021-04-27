f4ce0cb0375232cd8eb34de7df0e58ab21e6218e
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2021-04-27 10:45:11 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 while True:
     print("Rotation Level:" + input.rotation(Rotation.PITCH))
-    print("Sound Level:" + input.sound())
-    if input.rotation(Rotation.PITCH) > 25 :
+    print("Sound Level:" + input.sound_level())
+    if input.rotation(Rotation.PITCH) > 25 or input.rotation(Rotation.Pitch) < -25:
+        print("You have passed the Angle Limit")
+        pause(1000)
+        print("Go back up")
+        music.ba_ding.play_until_done()
         light.set_all(light.rgb(255, 0, 0))
+        pause(1000)
+    else:
+        light.set_all(light.rgb(0, 255, 0))
+
--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 while (true) {
     console.log("Rotation Level:" + input.rotation(Rotation.Pitch))
-    console.log("Sound Level:" + input.soundLevel)
-    if (input.rotation(Rotation.Pitch) > 25) {
+    console.log("Sound Level:" + input.soundLevel())
+    if (input.rotation(Rotation.Pitch) > 25 || input.rotation(Rotation.Pitch) < -25) {
+        console.log("You have passed the Angle Limit")
+        pause(1000)
+        console.log("Go back up")
+        music.baDing.playUntilDone()
         light.setAll(light.rgb(255, 0, 0))
+        pause(1000)
+    } else {
+        light.setAll(light.rgb(0, 255, 0))
     }
     
 }
