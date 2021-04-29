cd8bfc0f9c2d265577c4a4d4b77bdfbc981f1782
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2021-04-29 13:14:05 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,15 +1,34 @@
 while (true) {
     console.log("Rotation Level:" + input.rotation(Rotation.Pitch))
     console.log("Sound Level:" + input.soundLevel())
-    if (input.rotation(Rotation.Pitch) > 25 || input.rotation(Rotation.Pitch) < -25) {
+    if (input.rotation(Rotation.Pitch) > 25) {
         console.log("You have passed the Angle Limit")
         pause(1000)
         console.log("Go back up")
         music.baDing.playUntilDone()
-        light.setAll(light.rgb(255, 0, 0))
+        light.setPixelColor(0, light.rgb(255, 0, 0))
+        light.setPixelColor(1, light.rgb(255, 0, 0))
+        light.setPixelColor(2, light.rgb(255, 0, 0))
+        light.setPixelColor(3, light.rgb(255, 0, 0))
+        light.setPixelColor(4, light.rgb(255, 0, 0))
         pause(1000)
     } else {
-        light.setAll(light.rgb(0, 255, 0))
+        light.setAll(light.rgb(255, 255, 255))
+    }
+    
+    if (input.rotation(Rotation.Pitch) < -25) {
+        console.log("You have passed the Angle Limit")
+        pause(1000)
+        console.log("Go back up")
+        music.baDing.playUntilDone()
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+        light.setPixelColor(6, light.rgb(255, 0, 0))
+        light.setPixelColor(7, light.rgb(255, 0, 0))
+        light.setPixelColor(8, light.rgb(255, 0, 0))
+        light.setPixelColor(9, light.rgb(255, 0, 0))
+        pause(1000)
+    } else {
+        light.setAll(light.rgb(255, 255, 255))
     }
     
 }
