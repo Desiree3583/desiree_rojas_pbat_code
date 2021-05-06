f17335247ec78ec8d13c067bdd3dcc9ad7306eaf
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2021-05-05 20:00:18 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 while True:
     print("Rotation Level:" + input.rotation(Rotation.PITCH))
     print("Sound Level:" + input.sound_level())
-    if input.rotation(Rotation.PITCH) > 25 :
-        print("You have passed the Angle Limit")
-        pause(1000)
-        print("Go back up")
-        music.ba_ding.play_until_done()
-        light.set_pixel_color(0, light.rgb(255, 0, 0))
-        light.set_pixel_color(1, light.rgb(255, 0, 0))
-        light.set_pixel_color(2, light.rgb(255, 0, 0))
-        light.set_pixel_color(3, light.rgb(255, 0, 0))
-        light.set_pixel_color(4, light.rgb(255, 0, 0))
-        pause(1000)
-    else:
-        light.set_all(light.rgb(255, 255, 255))
-    if input.rotation(Rotation.PITCH) < -25 :
+    if input.rotation(Rotation.PITCH) > 74 :
         print("You have passed the Angle Limit")
         pause(1000)
         print("Go back up")
@@ -24,6 +11,19 @@ while True:
         light.set_pixel_color(7, light.rgb(255, 0, 0))
         light.set_pixel_color(8, light.rgb(255, 0, 0))
         light.set_pixel_color(9, light.rgb(255, 0, 0))
+        pause(1000)
+    else:
+        light.set_all(light.rgb(255, 255, 255))
+    if input.rotation(Rotation.PITCH) < -74 :
+        print("You have passed the Angle Limit")
+        pause(1000)
+        print("Go back up")
+        music.ba_ding.play_until_done()
+        light.set_pixel_color(0, light.rgb(255, 0, 0))
+        light.set_pixel_color(1, light.rgb(255, 0, 0))
+        light.set_pixel_color(2, light.rgb(255, 0, 0))
+        light.set_pixel_color(3, light.rgb(255, 0, 0))
+        light.set_pixel_color(4, light.rgb(255, 0, 0))
         pause(1000)
     else:
         light.set_all(light.rgb(255, 255, 255))
--- main.ts
+++ main.ts
@@ -1,22 +1,7 @@
 while (true) {
     console.log("Rotation Level:" + input.rotation(Rotation.Pitch))
     console.log("Sound Level:" + input.soundLevel())
-    if (input.rotation(Rotation.Pitch) > 25) {
-        console.log("You have passed the Angle Limit")
-        pause(1000)
-        console.log("Go back up")
-        music.baDing.playUntilDone()
-        light.setPixelColor(0, light.rgb(255, 0, 0))
-        light.setPixelColor(1, light.rgb(255, 0, 0))
-        light.setPixelColor(2, light.rgb(255, 0, 0))
-        light.setPixelColor(3, light.rgb(255, 0, 0))
-        light.setPixelColor(4, light.rgb(255, 0, 0))
-        pause(1000)
-    } else {
-        light.setAll(light.rgb(255, 255, 255))
-    }
-    
-    if (input.rotation(Rotation.Pitch) < -25) {
+    if (input.rotation(Rotation.Pitch) > 74) {
         console.log("You have passed the Angle Limit")
         pause(1000)
         console.log("Go back up")
@@ -26,6 +11,21 @@ while (true) {
         light.setPixelColor(7, light.rgb(255, 0, 0))
         light.setPixelColor(8, light.rgb(255, 0, 0))
         light.setPixelColor(9, light.rgb(255, 0, 0))
+        pause(1000)
+    } else {
+        light.setAll(light.rgb(255, 255, 255))
+    }
+    
+    if (input.rotation(Rotation.Pitch) < -74) {
+        console.log("You have passed the Angle Limit")
+        pause(1000)
+        console.log("Go back up")
+        music.baDing.playUntilDone()
+        light.setPixelColor(0, light.rgb(255, 0, 0))
+        light.setPixelColor(1, light.rgb(255, 0, 0))
+        light.setPixelColor(2, light.rgb(255, 0, 0))
+        light.setPixelColor(3, light.rgb(255, 0, 0))
+        light.setPixelColor(4, light.rgb(255, 0, 0))
         pause(1000)
     } else {
         light.setAll(light.rgb(255, 255, 255))
